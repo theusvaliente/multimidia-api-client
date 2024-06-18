a56d043846aad19cf6bd76eef0cef2abb404f3b6
Matheus Valiente Silva <matheus.valiente.mv@gmail.com>
2024-06-17 21:24:33 -0300

    feat: cria front resq api

--- components/card.js
+++ components/card.js
@@ -1,18 +1,34 @@
+// export default {
+//     name: 'Card',
+//     props: {
+//         cambio: {
+//             type: Object,
+//             required: true
+//         }
+//     },
+
+//     template:
+    // <div class="card">
+    //     <p class="card-title">{{ currency[converted].name }}</p>
+    //     <ul class="card-description">
+    //         <li>Moeda Base : {{ currency[converted].code }}</li>
+    //         <li>Moeda Referencia: {{ currency[converted].codein }}</li>
+    //         <li>Alta da Moeda: {{ currency[converted].high }}</li>
+    //     </ul>
+    // </div>
+//}
 export default {
     name: 'Card',
-    props: {
-        cambio: {
-            type: Object,
-            required: true
-        }
-    },
+    props: ['coffee'], 
+
     template: `
-    <div class="card">
-        <p class="card-title">{{ currency[converted].name }}</p>
-        <ul class="card-description">
-            <li>Moeda Base : {{ currency[converted].code }}</li>
-            <li>Moeda Referencia: {{ currency[converted].codein }}</li>
-            <li>Alta da Moeda: {{ currency[converted].high }}</li>
-        </ul>
-    </div>`
-}
+        <div class="card">
+            <p class="card-title">{{ coffee.nome }}</p>
+            <ul class="card-description">
+                <li>Aniversário: {{ coffee.aniverssario }}</li>
+                <li>CPF: {{ coffee.cpf }}</li>
+                <li>Café Preferido: {{ coffee.cafepreferido }}</li>
+            </ul>
+        </div>
+    `
+};
